3d4d0ef44b20400b2a9e60f0ac82f21c0c191eef
n-sassa <sshr2943@gmail.com>
2022-04-24 21:43:11 +0900

    旧ソースのコミット（動作確認済み）

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate, post_save
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        from core.signals import (
+            create_default_exercises,
+            create_user_profile,
+            change_exercise_flag,
+        )
+        from .models import User, ExerciseLog
+
+        post_migrate.connect(create_default_exercises, sender=self)
+        post_save.connect(create_user_profile, sender=User)
+        post_save.connect(change_exercise_flag, sender=ExerciseLog)
--- training_adviser/urls.py
+++ training_adviser/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from core.views import (
+    CreateUserView,
+    ProfileViewSet,
+    ExerciseViewSet,
+    ExerciseLogViewSet,
+    NextExerciseView,
+)
+
+router = routers.DefaultRouter()
+
+router.register("profile", ProfileViewSet, basename="profile")
+router.register("exercises", ExerciseViewSet, basename="exercises")
+router.register("exerciselog", ExerciseLogViewSet, basename="exerciselog")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/create/", CreateUserView.as_view(), name="create"),
+    path("api/v1/", include(router.urls)),
+    path("api/v1/next-exercise/", NextExerciseView.as_view(), name="next-exercise"),
+    path("auth/", include("djoser.urls.jwt")),
 ]
