2f19e730a4ef453d01b05fb87c63fa36143a5e4a
n-sassa <sshr2943@gmail.com>
2022-06-14 09:12:50 +0900

    単体テストお試し、コメント追加等

--- training_adviser/settings.py
+++ training_adviser/settings.py
@@ -102,6 +102,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        # sqlite3でテストする場合はインメモリDBなのでいらないかも
+        # "TEST": {
+        #     "NAME": "testdb",
+        # },
     }
 }
 
