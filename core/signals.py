0afa72b4fcafa406907f9261333bb46292c732ed
n-sassa <sshr2943@gmail.com>
2022-06-20 07:58:28 +0900

    テスト追加

--- core/signals.py
+++ core/signals.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from .constants import ExerciseSettings
 from .models import Exercise, ExerciseLog, Profile
 
@@ -18,13 +20,30 @@ def create_user_profile(sender, instance, created, **kwargs):
 def change_exercise_flag(sender, instance, created, **kwargs):
     if created:
         exercise_date = instance.exercise_date
-        queryset_count = (
-            ExerciseLog.objects.select_related("user")
-            .filter(user=instance.user, exercise_date=exercise_date)
-            .count()
+        exercise_log = (
+            ExerciseLog.objects.select_related("exercise")
+            .filter(
+                user=instance.user,
+                exercise_date=exercise_date,
+            )
+            .values_list("exercise__exercise_type")
         )
 
-        if queryset_count == 3:
+        # トレーニングタイプがA かつ 3種目登録されたらトレーニングフラグをTrueにする
+        exercise_type_a = [x for x in exercise_log if "A" in x[0]]
+
+        if len(exercise_type_a) == 3:
             user_profile = Profile.objects.get(user=instance.user)
-            user_profile.exercise_flag = not user_profile.exercise_flag
+            user_profile.exercise_flag = True
             user_profile.save()
+            return
+
+        # トレーニングタイプがB かつ ３種目登録されたらトレーニングフラグをFalseにする
+        exercise_type_b = [x for x in exercise_log if "B" in x[0]]
+
+        if len(exercise_type_b) == 3:
+            user_profile = Profile.objects.get(user=instance.user)
+            user_profile.exercise_flag = False
+            user_profile.save()
+            return
+        return
--- core/tests/test_api/test_next_exercise_view.py
+++ core/tests/test_api/test_next_exercise_view.py
@@ -28,7 +28,7 @@ def test_get_next_exercise_first(user_factory):
 
 
 @pytest.mark.django_db
-def test_get_next_exercise_second(user_factory, exercise_log_factory):
+def test_get_next_exercise_second(user_factory):
     """getのテスト 前回のトレーニングタイプがAの場合"""
     user = user_factory.create()
     client.force_authenticate(user)
@@ -39,11 +39,181 @@ def test_get_next_exercise_second(user_factory, exercise_log_factory):
 
     url = "/api/v1/next-exercise/"
     response = client.get(url)
-    profile = Profile.objects.get(user=user)
     assert response.status_code == status.HTTP_200_OK
+    # DBから取得し直す
+    profile.refresh_from_db()
     assert profile.exercise_flag
     data = response.data
     assert len(data) == 3
     assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
     assert data[1]["name"] == ExerciseSettings.SHOULDER_PRESS["name"]
     assert data[2]["name"] == ExerciseSettings.DEAD_LIFT["name"]
+
+
+@pytest.mark.django_db
+def test_get_next_exercise_before_exercise_type_b(user_factory, exercise_log_factory):
+    """getのテスト 前回のトレーニングタイプがBの場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    exercise = Exercise.objects.filter(exercise_type__contains="B").all()
+
+    _ = exercise_log_factory.create(user=user, exercise=exercise[0])
+    _ = exercise_log_factory.create(user=user, exercise=exercise[1])
+    _ = exercise_log_factory.create(user=user, exercise=exercise[2])
+
+    profile = Profile.objects.get(user=user)
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    # DBから取得し直す
+    profile.refresh_from_db()
+    assert not profile.exercise_flag
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert data[1]["name"] == ExerciseSettings.BENCH_PRESS["name"]
+    assert data[2]["name"] == ExerciseSettings.BARBELL_ROWING["name"]
+
+
+@pytest.mark.django_db
+def test_get_next_exercise_before_exercise_set_weight_achieved_type_a(
+    user_factory, exercise_log_factory
+):
+    """getのテスト 前回のトレーニングタイプAが目標達成している場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    exercise = Exercise.objects.filter(exercise_type__contains="A").all()
+
+    squat = exercise_log_factory.create(user=user, exercise=exercise[0])
+    bench_press = exercise_log_factory.create(user=user, exercise=exercise[1])
+    barbell_rowing = exercise_log_factory.create(user=user, exercise=exercise[2])
+
+    # テストのため強制的にフラグをFalseにする
+    profile = Profile.objects.get(user=user)
+    profile.exercise_flag = False
+    profile.save()
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert Decimal(data[0]["set_weight"]) == squat.set_weight + Decimal(2.5)
+    assert data[1]["name"] == ExerciseSettings.BENCH_PRESS["name"]
+    assert Decimal(data[1]["set_weight"]) == bench_press.set_weight + Decimal(2.5)
+    assert data[2]["name"] == ExerciseSettings.BARBELL_ROWING["name"]
+    assert Decimal(data[2]["set_weight"]) == barbell_rowing.set_weight + Decimal(2.5)
+
+
+@pytest.mark.django_db
+def test_get_next_exercise_before_exercise_not_set_weight_achieved_type_a(
+    user_factory, exercise_log_factory
+):
+    """getのテスト 前回のトレーニングタイプAが目標達成していない場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    exercise = Exercise.objects.filter(exercise_type__contains="A").all()
+
+    squat = exercise_log_factory.create(user=user, exercise=exercise[0], five_set=3)
+    bench_press = exercise_log_factory.create(
+        user=user, exercise=exercise[1], five_set=3
+    )
+    barbell_rowing = exercise_log_factory.create(
+        user=user, exercise=exercise[2], five_set=3
+    )
+
+    # テストのため強制的にフラグをFalseにする
+    profile = Profile.objects.get(user=user)
+    profile.exercise_flag = False
+    profile.save()
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert Decimal(data[0]["set_weight"]) == squat.set_weight
+    assert data[1]["name"] == ExerciseSettings.BENCH_PRESS["name"]
+    assert Decimal(data[1]["set_weight"]) == bench_press.set_weight
+    assert data[2]["name"] == ExerciseSettings.BARBELL_ROWING["name"]
+    assert Decimal(data[2]["set_weight"]) == barbell_rowing.set_weight
+
+
+@pytest.mark.django_db
+def test_get_next_exercise_before_exercise_set_weight_achieved_type_b(
+    user_factory, exercise_log_factory
+):
+    """getのテスト 前回のトレーニングタイプBが目標達成している場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    exercise = Exercise.objects.filter(exercise_type__contains="B").all()
+
+    squat = exercise_log_factory.create(user=user, exercise=exercise[0])
+    shoulder_press = exercise_log_factory.create(user=user, exercise=exercise[1])
+    dead_lift = exercise_log_factory.create(user=user, exercise=exercise[2])
+
+    # テストのため強制的にフラグをFalseにする
+    profile = Profile.objects.get(user=user)
+    profile.exercise_flag = True
+    profile.save()
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert Decimal(data[0]["set_weight"]) == squat.set_weight + Decimal(2.5)
+    assert data[1]["name"] == ExerciseSettings.SHOULDER_PRESS["name"]
+    assert Decimal(data[1]["set_weight"]) == shoulder_press.set_weight + Decimal(2.5)
+    assert data[2]["name"] == ExerciseSettings.DEAD_LIFT["name"]
+    assert Decimal(data[2]["set_weight"]) == dead_lift.set_weight + Decimal(5.0)
+
+
+@pytest.mark.django_db
+def test_get_next_exercise_before_exercise_not_set_weight_achieved_type_b(
+    user_factory, exercise_log_factory
+):
+    """getのテスト 前回のトレーニングタイプBが目標達成していない場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    exercise = Exercise.objects.filter(exercise_type__contains="B").all()
+
+    squat = exercise_log_factory.create(user=user, exercise=exercise[0], five_set=3)
+    shoulder_press = exercise_log_factory.create(
+        user=user, exercise=exercise[1], five_set=3
+    )
+    dead_lift = exercise_log_factory.create(
+        user=user,
+        exercise=exercise[2],
+        one_set=1,
+        two_set=1,
+        three_set=1,
+        four_set=1,
+        five_set=0,
+    )
+
+    # テストのため強制的にフラグをFalseにする
+    profile = Profile.objects.get(user=user)
+    profile.exercise_flag = True
+    profile.save()
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert Decimal(data[0]["set_weight"]) == squat.set_weight
+    assert data[1]["name"] == ExerciseSettings.SHOULDER_PRESS["name"]
+    assert Decimal(data[1]["set_weight"]) == shoulder_press.set_weight
+    assert data[2]["name"] == ExerciseSettings.DEAD_LIFT["name"]
+    assert Decimal(data[2]["set_weight"]) == dead_lift.set_weight
