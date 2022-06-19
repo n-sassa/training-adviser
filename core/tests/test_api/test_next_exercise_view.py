cb33f37bac2e0db593706d9d947143bdacf64613
n-sassa <sshr2943@gmail.com>
2022-06-19 21:51:49 +0900

    追加修正

--- core/tests/test_api/test_next_exercise_view.py
+++ core/tests/test_api/test_next_exercise_view.py
@@ -6,14 +6,14 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.constants import ExerciseSettings
-from core.models import Profile
+from core.models import Profile, Exercise
 
 client = APIClient()
 fake = Faker()
 
 
 @pytest.mark.django_db
-def test_get_next_exercise(user_factory):
+def test_get_next_exercise_first(user_factory):
     """getのテスト まだ何も登録がない場合"""
     user = user_factory.create()
     client.force_authenticate(user)
@@ -23,28 +23,27 @@ def test_get_next_exercise(user_factory):
     data = response.data
     assert len(data) == 3
     assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
-    assert Decimal(data[0]["set_weight"]) == Decimal(20)
     assert data[1]["name"] == ExerciseSettings.BENCH_PRESS["name"]
-    assert Decimal(data[1]["set_weight"]) == Decimal(20)
     assert data[2]["name"] == ExerciseSettings.BARBELL_ROWING["name"]
-    assert Decimal(data[2]["set_weight"]) == Decimal(20)
 
 
-# @pytest.mark.django_db
-# def test_get_next_exercise(user_factory, exercise_log_factory):
-#     """getのテスト 前回のトレーニングタイプがAの場合"""
-#     user = user_factory.create()
-#     client.force_authenticate(user)
-#     url = "/api/v1/next-exercise/"
-#     response = client.get(url)
-#     profile = Profile.objects.get(user=user)
-#     assert response.status_code == status.HTTP_200_OK
-#     assert profile.exercise_flag
-#     data = response.data
-#     assert len(data) == 3
-#     assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
-#     assert Decimal(data[0]["set_weight"]) == Decimal(22.5)
-#     assert data[1]["name"] == ExerciseSettings.SHOULDER_PRESS["name"]
-#     assert Decimal(data[1]["set_weight"]) == Decimal(20)
-#     assert data[2]["name"] == ExerciseSettings.DEAD_LIFT["name"]
-#     assert Decimal(data[2]["set_weight"]) == Decimal(40)
+@pytest.mark.django_db
+def test_get_next_exercise_second(user_factory, exercise_log_factory):
+    """getのテスト 前回のトレーニングタイプがAの場合"""
+    user = user_factory.create()
+    client.force_authenticate(user)
+
+    profile = Profile.objects.get(user=user)
+    profile.exercise_flag = True
+    profile.save()
+
+    url = "/api/v1/next-exercise/"
+    response = client.get(url)
+    profile = Profile.objects.get(user=user)
+    assert response.status_code == status.HTTP_200_OK
+    assert profile.exercise_flag
+    data = response.data
+    assert len(data) == 3
+    assert data[0]["name"] == ExerciseSettings.SQUAT["name"]
+    assert data[1]["name"] == ExerciseSettings.SHOULDER_PRESS["name"]
+    assert data[2]["name"] == ExerciseSettings.DEAD_LIFT["name"]
